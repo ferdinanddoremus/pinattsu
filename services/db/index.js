caeb9301bf98b49de4222b61bb2ee33738173faf
Grorémus <ferdinanddoremus@gmail.com>
2023-02-23 08:48:38 +0100

    service account in env file

--- services/db/index.js
+++ services/db/index.js
@@ -1,10 +1,22 @@
 import admin from 'firebase-admin'
-import serviceAccount from './serviceAccountKey.json'
+
+const serviceAccount = {
+  type: process.env.GSA_TYPE,
+  project_id: process.env.GSA_PROJECT_ID,
+  private_key_id: process.env.GSA_PRIVATE_KEY_ID,
+  private_key: process.env.GSA_PRIVATE_KEY,
+  client_email: process.env.GSA_CLIENT_EMAIL,
+  client_id: process.env.GSA_CLIENT_ID,
+  auth_uri: process.env.GSA_AUTH_URI,
+  token_uri: process.env.GSA_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GSA_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GSA_CLIENT_X509_CERT_URL,
+}
 
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount)
+      credential: admin.credential.cert(serviceAccount),
     })
   } catch (error) {
     console.log('Firebase admin initialization error', error.stack)
